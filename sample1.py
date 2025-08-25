e888644eafea14612b4c597e9ce29d4713d36656
Vaishnavi2622 <vaishnavivishwanath645@gmail.com>
2025-08-25 16:45:51 +0530

    Create sample1.py

---
+++ sample1.py
@@ -0,0 +1,9 @@
+student_name='vaishnavi'
+cell_phone=3456
+print(student_name)
+# 3 ways
+print('Hi',student_name,'welcome')
+print('Hi '+student_name+' welcome') # concatenation
+print(f'Hi {student_name} welcome') #formatted string
+print(f'Her phone number is {cell_phone}')
+
